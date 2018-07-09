bdfd07e1438f03cd7119e9d7e03bac70ea8c5590
salees1 <salees1@Ali-Dev-EDT.z2ybjzs0vpwutdjtjre0jwxzkb.ax.internal.cloudapp.net>
2018-07-09 16:24:30 +0100

    course code

--- Test/src/main/main.go
+++ Test/src/main/main.go
@@ -4,11 +4,28 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
 func main() {
-	fmt.Println("Hello World")
-
-	for i, v := range []int{1, 2, 3, 4} {
-		fmt.Println(i, v)
-
-	}
+	ali := person{"Ali", "Saleem", contactInfo{"ali.saleem@gmail.com", 12345}}
+	aliPtr := &ali
+	aliPtr.updateFirstName("Aly")
+	ali.print()
+}
+func (ptrToPerson *person) updateFirstName(firstName string) {
+	(ptrToPerson).firstName = firstName
+	fmt.Println(*ptrToPerson)
+	fmt.Println("****")
+	fmt.Println(ptrToPerson)
+}
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
